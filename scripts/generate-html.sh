9b78af086a9fe95968410851e2726f9438765b81
Francesco Saccone <francesco@francescosaccone.com>
2025-06-14 15:44:35 +0200

    feat: create IGNORED_FILES and ignore 404.md and 5xx.md
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- scripts/generate-html.sh
+++ scripts/generate-html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for program in cat dirname echo find lowdown mkdir rm realpath;  do
+for program in cat dirname echo find grep lowdown mkdir rm realpath;  do
   if ! command -v "$program" > /dev/null 2>&1; then
     echo "Error: Required program '$program' is not installed."
     exit 1
@@ -16,8 +16,13 @@ SOURCE="$(realpath $(dirname $0)/..)"
 DESTINATION="$1"
 HEADER="$SOURCE/header.html"
 FOOTER="$SOURCE/footer.html"
+IGNORED_FILES="$SOURCE/404.md $SOURCE/5xx.md"
 
 for md_file in $(find "$SOURCE" -type f -name "*.md"); do
+  if echo "$IGNORED_FILES" | grep -qw "$md_file"; then
+    continue
+  fi
+
   relative_path="${md_file#$SOURCE/}"
   html_file="$DESTINATION/${relative_path%.md}.html"
 
